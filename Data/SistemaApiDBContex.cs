67bcd2bea5a188df8c61bff61b704a207f49db97
Gabriel Alves <alvesg2003@gmail.com>
2023-01-20 21:29:32 -0300

    create restAPI

---
+++ Data/SistemaApiDBContex.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaAPI.Data.Map;
+using SistemaAPI.Model;
+
+namespace SistemaAPI.Data
+{
+    public class SistemaApiDBContex : DbContext
+    {
+        public SistemaApiDBContex(DbContextOptions<SistemaApiDBContex> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Model/TarefaModel.cs
@@ -0,0 +1,15 @@
+﻿using SistemaAPI.Enums;
+
+namespace SistemaAPI.Model
+{
+    public class TarefaModel
+    {
+        public int Id { get; set; }
+
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+
+        public StatusTarefa Status { get; set; }
+
+    }
+}
